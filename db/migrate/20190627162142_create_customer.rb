cc9fd0457826f3b45d5c0c08a10b0da9dd6ce0d4
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-06-27 14:28:25 -0600

    resolved issues with migrations, migrated and ran rake task

--- db/migrate/20190627162142_create_customer.rb
+++ db/migrate/20190627162142_create_customer.rb
@@ -1,7 +1,7 @@
 class CreateCustomer < ActiveRecord::Migration[5.1]
   def change
     create_table :customers do |t|
-      t.string :first__name
+      t.string :first_name
       t.string :last_name
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20190627164943) do
   enable_extension "plpgsql"
 
   create_table "customers", force: :cascade do |t|
-    t.string "first__name"
+    t.string "first_name"
     t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
